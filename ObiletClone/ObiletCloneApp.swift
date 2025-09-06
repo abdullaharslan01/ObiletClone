cb9d980cb27267806ab34ee8a417dac9bba5602b
Abdullah Arslan <abdullaharslandev@gmail.com>
2025-09-06 18:35:10 +0300

    Initial Commit

---
+++ ObiletClone/ObiletCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObiletCloneApp.swift
+//  ObiletClone
+//
+//  Created by abdullah on 06.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct ObiletCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
